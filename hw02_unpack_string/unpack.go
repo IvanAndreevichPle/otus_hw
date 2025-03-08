d314ca8f42a7fc969248696606f10009d1e8fd32
Ivan <ivanandreevic@yandex-team.ru>
2025-03-08 16:23:04 +0000

    unpacking string method with additional check

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,62 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var result strings.Builder
+	var previousChar rune
+	var isShielded bool
+	var isPreviousDigit bool
+
+	for _, char := range s {
+		if unicode.IsDigit(char) && !isShielded {
+			if previousChar == 0 || isPreviousDigit {
+				return "", ErrInvalidString
+			}
+			count, err := strconv.Atoi(string(char))
+			if err != nil {
+				return "", errors.New("failed to convert character to integer")
+			}
+			handleDigit(&result, previousChar, count)
+			isPreviousDigit = true
+		} else {
+			handleChar(&result, char, &isShielded)
+			previousChar = char
+			isPreviousDigit = false
+		}
+	}
+
+	if isShielded {
+		return "", ErrInvalidString
+	}
+
+	return result.String(), nil
+}
+
+func handleDigit(result *strings.Builder, previousChar rune, count int) {
+	if count == 0 {
+		str := result.String()
+		result.Reset()
+		result.WriteString(str[:len(str)-len(string(previousChar))])
+	} else {
+		result.WriteString(strings.Repeat(string(previousChar), count-1))
+	}
+}
+
+func handleChar(result *strings.Builder, char rune, isShielded *bool) {
+	switch {
+	case *isShielded:
+		result.WriteRune(char)
+		*isShielded = false
+	case char == '\\':
+		*isShielded = true
+	default:
+		result.WriteRune(char)
+	}
 }
