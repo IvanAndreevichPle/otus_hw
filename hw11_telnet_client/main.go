f36a33f6787c4796dc06d0c5641b61d5cf10db52
Ivan <ivanandreevic@yandex-team.ru>
2025-07-05 06:18:19 +0000

    "HW11 is completed"

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,55 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.Duration("timeout", 10*time.Second, "connection timeout")
+	flag.Parse()
+
+	if flag.NArg() != 2 {
+		fmt.Fprintln(os.Stderr, "usage: go-telnet [--timeout=10s] host port")
+		os.Exit(2)
+	}
+	host, port := flag.Arg(0), flag.Arg(1)
+	address := net.JoinHostPort(host, port)
+
+	client := NewTelnetClient(address, *timeout, os.Stdin, os.Stdout)
+	if err := client.Connect(); err != nil {
+		fmt.Fprintf(os.Stderr, "...Connection error: %v\n", err)
+		os.Exit(1)
+	}
+	defer client.Close()
+	fmt.Fprintf(os.Stderr, "...Connected to %s\n", address)
+
+	// Обработка SIGINT (Ctrl+C)
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	done := make(chan struct{})
+	var once sync.Once
+
+	go func() {
+		_ = client.Receive()
+		once.Do(func() { close(done) })
+	}()
+	go func() {
+		_ = client.Send()
+		once.Do(func() { close(done) })
+	}()
+
+	select {
+	case <-done:
+		fmt.Fprintln(os.Stderr, "...Connection was closed by peer or EOF")
+	case <-sigCh:
+		fmt.Fprintln(os.Stderr, "...Interrupted")
+	}
 }
